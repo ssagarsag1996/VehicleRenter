d59185b526f6c1314162cb1456a6c10fa90b86aa
Sagar <ssagar.sag1996@gmail.com>
2021-10-19 22:19:10 +0530

    Add project files.

---
+++ VehicleRenter_UI/ApiControllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleRenter_BL;
+
+namespace VehicleRenter_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration configuration;
+        private readonly IDbConnection db = SqlHelper.Open();
+        public WeatherForecastController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+         {
+            try
+            {
+                var rng = new Random();
+                var Result= Enumerable.Range(1, 20).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToList();
+
+                return new JsonResult(new { Message = "", Result = Result });
+            }
+            catch(Exception ex)
+            {
+                return new JsonResult(new { Message = "", Result = ex.Message });
+            }
+        }
+    }
+}
---
+++ VehicleRenter_UI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Http;
+using VehicleRenter_API;
+using VehicleRenter_BL;
+using VehicleRenter_UI.Models;
+using Newtonsoft.Json;
+
+namespace VehicleRenter_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly IDbConnection db = SqlHelper.Open();
+        private readonly String Baseurl = "https://localhost:44370/";
+
+        public HomeController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
